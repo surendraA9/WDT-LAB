c54510f279999a5ee226cbb378d248cfbbb1ecb2
surendraA9 <143076077+surendraA9@users.noreply.github.com>
2023-10-19 16:05:44 +0530

    Delete 10 directory

--- 10/delete-inline.php
+++
@@ -1,8 +0,0 @@
-<?php
-$stu_id = $_GET['id'];
-include 'config.php';
-$sql = "DELETE FROM student WHERE sid = {$stu_id}";
-$result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
-header("Location: http://localhost/Experiment10/index.php");
-mysqli_close($conn);
-?>
